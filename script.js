a55ffe6c8e48bc1e3ffbea3d73ab5852215031a8
Leorobin396 <157003317+Leorobin396@users.noreply.github.com>
2024-02-28 00:35:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var elem = document.querySelectorAll(".elem")
+
+elem.forEach(function(val){
+
+    val.addEventListener("mouseenter",function(){
+        val.childNodes[3].style.opacity=1
+    });
+    val.addEventListener("mouseleave",function(){
+        val.childNodes[3].style.opacity=0
+    });
+    // val.addEventListener("mousemove",function(dets){
+    //     val.childNodes[3].style.left=(dets.x+10)+"px"
+    //     val.childNodes[3].style.top=(dets.y+10)+"px"
+    // });
+    val.addEventListener("mousemove", function(dets) {
+        val.childNodes[3].style.position = "fixed";
+        val.childNodes[3].style.left = dets.clientX + "px";
+        val.childNodes[3].style.top = dets.clientY + "px";
+    });
+    
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var elemImage = document.querySelector("#elem1 imag")
+
+// elem1.addEventListener("mousemove",function(dets){
+//     elemImage.style.left=dets.x+"px"
+//     elemImage.style.top=dets.y+"px"
+// })
+
+// elem1.addEventListener("mouseenter",function(dets){
+//     elemImage.style.opacity=1
+// })
+
+// elem1.addEventListener("mouseleave",function(dets){
+//     elemImage.style.opacity=0
+// })
